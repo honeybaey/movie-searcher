ef9bfefa682e6b9af0b81c18d387d177514854fe
honeybaey <honeybaey@gmail.com>
2020-07-07 19:06:52 +0300

    adding movie card component

---
+++ src/components/MovieCard/MovieCard.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./MovieCard.scss";
+
+const MovieCard = () => {
+  return (
+    <div className="movie-item">
+      <img
+        src="https://image.tmdb.org/t/p/w200/cxKeSarIhy2QmjwObAcJ0wlduJT.jpg"
+        alt="Acting on Impulse"
+      />
+      <p className="movie-card__title">Acting on Impulse</p>
+    </div>
+  );
+};
+
+export default MovieCard;
