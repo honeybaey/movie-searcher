2bdb1e2b477952ac23590a91c04d25fc11374b6c
honeybaey <honeybaey@gmail.com>
2020-09-17 21:05:11 +0300

    added display of data form the api

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,16 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import MovieService from "../../services/MovieService";
+
 import "./MovieCard.scss";
 
-const MovieCard = () => {
-  return (
-    <div className="movie-card">
-      <img
-        src="https://image.tmdb.org/t/p/w200/cxKeSarIhy2QmjwObAcJ0wlduJT.jpg"
-        alt="Acting on Impulse"
-      />
-      <p className="movie-card__title">Acting on Impulse</p>
-    </div>
-  );
-};
+export default class MovieCard extends Component {
+  movieService = new MovieService();
 
-export default MovieCard;
+  constructor() {
+    super();
+
+    this.state = {
+      img: null,
+      alt: "",
+      title: "",
+    };
+
+    // this.updateFilm();
+  }
+
+  componentDidMount() {
+    this.updateFilm();
+  }
+
+  updateFilm() {
+    this.movieService.getFilm(136).then((film) => {
+      this.setState({
+        img: film.poster_path,
+        alt: film.title,
+        title: film.title,
+      });
+      console.log(film)
+    });
+  }
+
+  render() {
+    const { img, alt, title } = this.state;
+
+    return (
+      <div className="movie-card">
+        <img src={`https://image.tmdb.org/t/p/w200${img}`} alt={alt} />
+        <p className="movie-card__title">{title}</p>
+      </div>
+    );
+  }
+}
