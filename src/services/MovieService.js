32ad7b6bf0968c49a26f6f440033f44911b22deb
honeybaey <honeybaey@gmail.com>
2020-09-18 20:57:22 +0300

    changed the person search function

--- src/services/MovieService.js
+++ src/services/MovieService.js
@@ -1,11 +1,9 @@
 export default class MovieService {
   _apiKey = "9000047236c3b4a86957e392cead86b6";
-  _query = "Дикаприо"; // пример
-  _urlSearch = `https://api.themoviedb.org/3/search/person?api_key=${this._apiKey}&language=ru-RU&query=${this._query}`;
-  _urlFilms = `https://api.themoviedb.org/3`;
+  _baseURL = `https://api.themoviedb.org/3`;
 
   async getResource(url) {
-    const res = await fetch(`${this._urlFilms}${url}`);
+    const res = await fetch(`${this._baseURL}${url}`);
 
     if (!res.ok) {
       throw new Error(`Couldn't fetch ${url}, received ${res.status}`);
@@ -19,14 +17,8 @@ export default class MovieService {
     return res;
   }
 
-  /* async searchPerson() {
-    const res = await this.getResource();
+  async getPerson(query) {
+    const res = await this.getResource(`/search/person?api_key=${this._apiKey}&language=ru-RU&query=${query}`);
     return res.results;
-  } */
+  }
 }
-
-// const person = new MovieService();
-
-// person.searchPerson().then((res) => {
-//   console.log(res);
-// });
