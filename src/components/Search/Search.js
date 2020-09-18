2bb6352a86920c5aa67a16ed6a07e6e56542d239
honeybaey <honeybaey@gmail.com>
2020-09-18 20:59:18 +0300

    added query processing and sending

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,12 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Search.scss";
 
-const Search = () => {
-  return (
-    <form className='search-form'>
-      <input type="text" className='search-input' placeholder='Enter the name' />
-    </form>
-  );
-};
+export default class Search extends Component {
+  constructor() {
+    super();
 
-export default Search;
+    this.state = {
+      query: "",
+    };
+
+    this.onEnterName = (e) => {
+      this.setState({
+        query: e.target.value,
+      });
+    };
+
+    this.onSubmit = (e) => {
+      e.preventDefault();
+      this.props.onSearchPerson(this.state.query);
+      this.setState({
+        query: "",
+      });
+    };
+  }
+
+  render() {
+    return (
+      <form className="search-form" onSubmit={this.onSubmit}>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Введите фамилию"
+          onChange={this.onEnterName}
+          value={this.state.query}
+        />
+      </form>
+    );
+  }
+}
