78575e046575b136b36bee0851887dd866bf50b5
honeybaey <honeybaey@gmail.com>
2020-09-18 21:00:30 +0300

    moved logic to App

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,44 +1,15 @@
 import React, { Component } from "react";
-import MovieService from "../../services/MovieService";
 
 import "./MovieCard.scss";
 
 export default class MovieCard extends Component {
-  movieService = new MovieService();
-
-  constructor() {
-    super();
-
-    this.state = {
-      img: null,
-      alt: "",
-      title: "",
-    };
-
-    // this.updateFilm();
-  }
-
-  componentDidMount() {
-    this.updateFilm();
-  }
-
-  updateFilm() {
-    this.movieService.getFilm(136).then((film) => {
-      this.setState({
-        img: film.poster_path,
-        alt: film.title,
-        title: film.title,
-      });
-      console.log(film)
-    });
-  }
 
   render() {
-    const { img, alt, title } = this.state;
+    const { img, title } = this.props;
 
     return (
       <div className="movie-card">
-        <img src={`https://image.tmdb.org/t/p/w200${img}`} alt={alt} />
+        <img src={`https://image.tmdb.org/t/p/w200${img}`} alt={title} />
         <p className="movie-card__title">{title}</p>
       </div>
     );
