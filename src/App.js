c4be4e53b79a65309622dd2b5d51f7e9cffd6829
honeybaey <honeybaey@gmail.com>
2020-09-21 12:16:32 +0300

    added display of found movies

--- src/App.js
+++ src/App.js
@@ -1,19 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./components/Header/Header";
 import Search from "./components/Search/Search";
+import MovieService from "./services/MovieService";
+import MovieList from "./components/MovieList/MovieList";
+import About from "./components/About/About";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <div className="container">
-        <Search />
-        <div className="about">
-          <p>Service for searching an actor's filmography by name</p>
+export default class App extends Component {
+  movieService = new MovieService();
+
+  constructor() {
+    super();
+
+    this.state = {
+      films: [],
+      isLoaded: false,
+    };
+
+    this.searchPerson = (query) => {
+      this.movieService.getPerson(query).then((person) => {
+        const film = person[0].known_for;
+        let filmsArr = [];
+        film.forEach((item) => {
+          filmsArr.push({ title: item.title, img: item.poster_path });
+        });
+        this.setState({
+          films: filmsArr,
+          isLoaded: true,
+        });
+      });
+    };
+  }
+
+  render() {
+    const { isLoaded, films } = this.state;
+    return (
+      <div>
+        <Header />
+        <div className="container">
+          <Search onSearchPerson={this.searchPerson} />
+          { !isLoaded ? <About /> : null }
+          <MovieList films={films} />
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
