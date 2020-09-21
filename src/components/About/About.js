204ead137d0a1ddf39de9de38e2696ceee415041
honeybaey <honeybaey@gmail.com>
2020-09-21 12:13:41 +0300

    moved component from App

---
+++ src/components/About/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.scss";
+
+const About = () => {
+  return (
+    <div className="about">
+      <p>
+        Сервис показывает топ-3 фильма, благодаря которым стал популярен актер или режиссер
+      </p>
+    </div>
+  );
+};
+
+export default About;
