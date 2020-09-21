e2635135cd0c9ab80eca2dee9652996499e9f132
honeybaey <honeybaey@gmail.com>
2020-09-21 13:14:51 +0300

    Updates

---
+++ precache-manifest.f1c360c6a2c75793fdb54c852451980e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc38acc466ed13aa1d37857acbf6ea6e",
+    "url": "/movie-searcher/index.html"
+  },
+  {
+    "revision": "c429e354554340d765ec",
+    "url": "/movie-searcher/static/css/main.fdaa84b2.chunk.css"
+  },
+  {
+    "revision": "146ed5deee29fe0ccc43",
+    "url": "/movie-searcher/static/js/2.e5cb2d85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-searcher/static/js/2.e5cb2d85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c429e354554340d765ec",
+    "url": "/movie-searcher/static/js/main.0fb16529.chunk.js"
+  },
+  {
+    "revision": "303e4df81a0a22141848",
+    "url": "/movie-searcher/static/js/runtime-main.fc9780ba.js"
+  },
+  {
+    "revision": "01976af06e178c5b3e31be36b4772761",
+    "url": "/movie-searcher/static/media/Modak.01976af0.woff"
+  },
+  {
+    "revision": "58308c4c9da25fd77360ab5682ac1aac",
+    "url": "/movie-searcher/static/media/Modak.58308c4c.woff2"
+  },
+  {
+    "revision": "e6847609663f6f336df1692aa367dc6b",
+    "url": "/movie-searcher/static/media/Modak.e6847609.ttf"
+  },
+  {
+    "revision": "025a9cece0543d700fe376b8482556b5",
+    "url": "/movie-searcher/static/media/Montserrat-Regular.025a9cec.woff2"
+  },
+  {
+    "revision": "e9949803916ea98bfcf50a2ebd4f826e",
+    "url": "/movie-searcher/static/media/Montserrat-Regular.e9949803.ttf"
+  },
+  {
+    "revision": "ffb135af26982b1e0cbf6700ea8898d3",
+    "url": "/movie-searcher/static/media/Montserrat-Regular.ffb135af.woff"
+  }
+]);
