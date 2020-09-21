445b65bfd99f052d921eab277c9bd1fea6de1c07
honeybaey <honeybaey@gmail.com>
2020-09-21 12:46:11 +0300

    added a component for displaying errors

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search/Search";
 import MovieService from "./services/MovieService";
 import MovieList from "./components/MovieList/MovieList";
 import About from "./components/About/About";
+import ErrorIndicator from "./components/ErrorIndicator/ErrorIndicator";
 
 export default class App extends Component {
   movieService = new MovieService();
@@ -14,33 +15,47 @@ export default class App extends Component {
     this.state = {
       films: [],
       isLoaded: false,
+      error: false,
+    };
+
+    this.onError = (err) => {
+      this.setState({
+        error: true,
+      });
     };
 
     this.searchPerson = (query) => {
-      this.movieService.getPerson(query).then((person) => {
-        const film = person[0].known_for;
-        let filmsArr = [];
-        film.forEach((item) => {
-          filmsArr.push({ title: item.title, img: item.poster_path });
-        });
-        this.setState({
-          films: filmsArr,
-          isLoaded: true,
-        });
-      });
+      this.movieService
+        .getPerson(query)
+        .then((person) => {
+          const film = person[0].known_for;
+          let filmsArr = [];
+          film.forEach((item) => {
+            filmsArr.push({ title: item.title, img: item.poster_path });
+          });
+          this.setState({
+            films: filmsArr,
+            isLoaded: true,
+          });
+        })
+        .catch(this.onError);
     };
   }
 
   render() {
-    const { isLoaded, films } = this.state;
+    const { isLoaded, films, error } = this.state;
     return (
       <div>
         <Header />
-        <div className="container">
-          <Search onSearchPerson={this.searchPerson} />
-          { !isLoaded ? <About /> : null }
-          <MovieList films={films} />
-        </div>
+        {
+          !error 
+          ? <div className="container">
+              <Search onSearchPerson={this.searchPerson} />
+              {!isLoaded ? <About /> : null}
+              <MovieList films={films} />
+            </div>
+          : <ErrorIndicator />
+        }
       </div>
     );
   }
